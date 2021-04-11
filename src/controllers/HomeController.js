f1ad0af45e8caad14558f2ad474f10540be41c24
Guirra <br.guirra@gmail.com>
2021-04-11 17:36:08 -0300

    Aula 180 - Rota de alunos

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,16 +1,6 @@
-import Aluno from '../models/Aluno';
-
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: 'Larissa',
-      sobrenome: 'Pereira',
-      email: 'larissa@gmail.com',
-      idade: 26,
-      peso: 60,
-      altura: 1.70,
-    });
-    res.json(novoAluno);
+    res.json('Index');
   }
 }
 

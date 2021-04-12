3705bba74e3642a91727f71bed2bdca3eec3c03d
Guirra <br.guirra@gmail.com>
2021-04-12 20:16:05 -0300

    Aula 183 - Tratando os arquivos enviados

--- src/controllers/FotoController.js
+++ src/controllers/FotoController.js
@@ -1,6 +1,20 @@
+import multer from 'multer';
+
+import multerConfig from '../config/multerConfig';
+
+const upload = multer(multerConfig).single('foto');
+
 class FotoController {
   async store(req, res) {
-    res.json(req.file);
+    upload(req, res, (error) => {
+      if (error) {
+        return res.status(401).json({
+          errors: [error.code],
+        });
+      }
+
+      return res.json(req.file);
+    });
   }
 }
 

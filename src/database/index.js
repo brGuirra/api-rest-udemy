3ead9d01efd84947e4e70c205c2814cdd31e6c0b
Guirra <br.guirra@gmail.com>
2021-04-12 22:01:23 -0300

    Aula 184 - Salvando a foto na base de dados

--- src/database/index.js
+++ src/database/index.js
@@ -1,13 +1,19 @@
 import Sequelize from 'sequelize';
+
 import databaseConfig from '../config/database';
+
 import Aluno from '../models/Aluno';
 import User from '../models/User';
+import Foto from '../models/Foto';
 
 // Array contendo os models
-const models = [Aluno, User];
+const models = [Aluno, User, Foto];
 
 // Cria uma connections com as configurações do db
 const connection = new Sequelize(databaseConfig);
 
 // Inicia a conexão com o db para cada model
 models.forEach((model) => model.init(connection));
+
+// Associa as chaves dos models
+models.forEach((model) => model.associate && model.associate(connection.models));
